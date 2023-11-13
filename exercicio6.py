5ea5bedb4b4875b1bd1fdcf3705c5de2f6b2baf8
manuely guedes <manuelyufpa@gmail.com>
2023-11-13 17:04:30 -0300

    Resolução exercicio 6.

---
+++ exercicio6.py
@@ -0,0 +1,22 @@
+
+#Solicitando nome do usuário
+print("Informe seu nome:")
+nome = input()
+
+#Utilização de while para manter a solicitação do ano de nascimento caso o usuário não informe de forma correta.
+while True:
+    #Usando try exception para capturar erros e informar ao usuário
+    try:
+        print("Informe o ano de nascimento entre 1922 e 2021:")
+        anoNascimento = int(input())
+
+        #Verificando se o ano digitado está entre o período permitido.
+        if(anoNascimento >= 1922 and anoNascimento <= 2021):
+            idade = 2022 - anoNascimento
+            print(f"{nome} você tem {idade} anos!")
+            break
+        else:
+            print("Ano está fora do período determinado!")
+    except:
+        print("Ano inválido!")
+
