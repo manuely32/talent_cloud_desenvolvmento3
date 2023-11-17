47b8277e5c76006eec71aff4d1851c7147be8bc5
manuely guedes <manuelyufpa@gmail.com>
2023-11-17 17:02:43 -0300

    Exercicio sobre array

---
+++ exercicio1Arrays.py
@@ -0,0 +1,7 @@
+#Array de produtos de loja de roupa
+lista_produtos_loja_roupa = ["Camisa", "Calça Jeans", "Saia", "Blusa", "Vestido"] 
+print("Produtos disponiveis na loja: ", lista_produtos_loja_roupa)
+
+#Array de datas de nascimento
+lista_ano_nascimento = ['01/12/1986', '17/12/1984', '02/05/2022', '03/04/2020', '24/07/2001']
+print("Datas de nascimento da familia: ", lista_ano_nascimento)
