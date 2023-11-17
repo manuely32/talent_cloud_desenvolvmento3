cbeadd7d1c36b9709f287be41deb897c4b605c12
manuely guedes <manuelyufpa@gmail.com>
2023-11-17 18:08:47 -0300

    Ajuste

--- exercicio4Arrays.py
+++ exercicio4Arrays.py
@@ -9,9 +9,12 @@ lista_produtos[4] = "cremes hidratantes"
 #removendo delineadores da lista
 lista_produtos.pop()
 
+#adicione dois novos produtos da sua escolha à lista. 
+lista_produtos.append("sombra")
+lista_produtos.append("desodorantes")
+
 for i in range(len(lista_produtos)):
     print(f"Temos {lista_produtos[i]} à venda!")
-
 
 #lista após as alterações
 print(lista_produtos)
