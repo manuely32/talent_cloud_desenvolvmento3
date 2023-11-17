07cdc8969b02d39c5d47c7df575956c34a057e7e
manuely guedes <manuelyufpa@gmail.com>
2023-11-17 17:20:33 -0300

    Exercicio 2 arrays.

---
+++ exercicio2Arrays.py
@@ -0,0 +1,8 @@
+#Copie o array apresentado embaixo no seu editor de código, e imprima no terminal: a quantidade de elementos que ele possui, o dado salvo no índice 2, o dado salvo no índice 9, e dado salvo no índice 14.
+
+lista_musicos = [ 'Djavan', 'Roberto Carlos', 'Elis Regina', 'Tom Jobim', 'Milton Nascimento', 'Chico Buarque', 'Nara Leão', 'Pitty', 'Simonal', 'Moacir Santos', 'Caetano Veloso', 'Elza Soares', 'Paulinho da Viola', 'Yamandú Costa', 'Gal Costa'] 
+
+print(f"O array possui {len(lista_musicos)} elementos")
+print("O dado salvo no indice 2 é :", lista_musicos[2])
+print("O dado salvo no indice 9 é :", lista_musicos[9])
+print("O dado salvo no indice 14 é :", lista_musicos[14])
